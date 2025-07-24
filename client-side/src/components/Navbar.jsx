ea3c24de356af0e1d2c16b16933321d6df9eae13
DJ-x501 <bhargavdhananjaix501@gmail.com>
2025-07-24 18:44:24 +0530

    new update

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -16,10 +16,11 @@ const Navbar = () => {
             <Link to="/" className='max-md:flex-1'>
                 <img src={assets.logo} alt='' className='w-16 h-auto' />
             </Link>
-            <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center
-        max-md:justify-center gap-8 px-4 mid-md:px-8 py-3 max-md:h-screen
-        min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10
-        md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
+            <div className={`max-md:fixed max-md:top-0 max-md:left-0 max-md:z-40 max-md:w-full max-md:h-screen max-md:bg-black/80
+    max-md:backdrop-blur-sm max-md:transition-all max-md:duration-300
+    ${isOpen ? 'max-md:translate-x-0' : 'max-md:-translate-x-full'}
+    flex flex-col md:flex-row items-center justify-center gap-8 md:static md:h-auto md:w-auto md:bg-transparent md:translate-x-0`}>
+
                 <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
                 <Link to="/" onClick={() => { scrollTo(0, 0); setIsOpen(false) }}>Home</Link>
                 <Link to="/movies" onClick={() => { scrollTo(0, 0); setIsOpen(false) }}>Movies</Link>
